4297b2b6bb1ddf2ddd17dc17e9155b17a957dbcd
LiMengSheng <927971135@qq.com>
2024-01-05 22:55:00 +0800

    upadte readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# java-multithread-program
-code in &lt;Java Multithread Programming 3nd>
+# Java Multithread Program
+ 《Java多线程编程核心技术-第三版》&lt;`Java Multithread Programming 3nd Edition`> 
+
+## chapter 1
+
+- 1.2 使用多线程
+- 1.3 `currentThread()`
+- 1.4 `isAlive()`
+- 1.5/1.6 `sleep()`
+- 1.10 `getId()`
+- 1.11 停止线程
+- 1.12 暂停线程
+
+## chapter 2
--- chapter1/src/main/java/isAlive/Test2.java
+++ chapter1/src/main/java/isAlive/Test2.java
@@ -1,11 +1,11 @@
 package isAlive;
 
-public class Test1 {
-    public static void main(String[] args) {
+public class Test2 {
+    public static void main(String[] args) throws InterruptedException{
         MyThread myThread = new MyThread();
         System.out.println("begin1==" + myThread.isAlive());
         myThread.start();
-        // uncertain value
+        Thread.sleep(2000);
         System.out.println("end==" + myThread.isAlive());
     }
 }
